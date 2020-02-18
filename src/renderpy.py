4d8aeb95faf289a41f8b9d1b77308ab8be7960fe
Anton Schreiner <antonschrein@gmail.com>
2020-02-18 18:03:40 +0000

    Continue python backend

--- src/renderpy.py
+++ src/renderpy.py
@@ -21,25 +21,45 @@ WIDTH, HEIGHT = 512, 512
 # mesh.show()
 
 
+# Attribute dictionary
+# used to hack python into making javascript like objects-dictionaries
+class AD(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+def replace_dict(d):
+  if isinstance(d, dict):
+    d = AD(d)
+    for key in d.keys():
+      d[key] = replace_dict(d[key])
+    return d
+  elif isinstance(d, list):
+    for i, item in enumerate(d):
+      d[i] = replace_dict(item)
+    return d
+  else:
+    return d
+  
+
 class Node:
   def __init__(self, global_state, json_node):
-    self.id = json_node["id"]
+    self.id = json_node.id
     global_state.id2node[self.id] = self
     self.global_state = global_state
-    self.title = json_node["title"]
+    self.title = json_node.title
     self.outputs = []
     self.inputs = []
     self.is_recursive = False
-    for o in json_node["outputs"]:
-      self.outputs.append(o)
-    for o in json_node["inputs"]:
-      self.inputs.append(o)
-    self.properties = json_node["properties"]
+    for o in json_node.outputs:
+      self.outputs.append(AD(o))
+    for o in json_node.inputs:
+      self.inputs.append(AD(o))
+    self.properties = replace_dict(json_node.properties)
 
   def get_output_by_name(self, name):
     i = 0
     for o in self.outputs:
-      if o["name"] == name:
+      if o.name == name:
         return (i, o)
       i += 1
     return None
@@ -47,21 +67,21 @@ class Node:
   def get_input_ids(self):
     out = set()
     for o in self.inputs:
-      link_id = o["link"]
+      link_id = o.link
       if link_id:
         link = self.global_state.id2link[link_id]
         out.add(link.origin_node_id)
     return out
 
   def get_input_node_by_slot(self, slot):
-    link_id = self.inputs[slot]["link"]
+    link_id = self.inputs[slot].link
     link = self.global_state.id2link[link_id]
     return self.global_state.id2node[link.origin_node_id]
 
   def get_input_nodes(self):
     out = []
     for o in self.inputs:
-      link_id = o["link"]
+      link_id = o.link
       if link_id:
         link = self.global_state.id2link[link_id]
         out.add(self.global_state.id2node[link.origin_node_id])
@@ -69,8 +89,8 @@ class Node:
 
   def getInputNodeByName(self, name):
     for o in self.inputs:
-      if o["name"] == name:
-        link_id = o["link"]
+      if o.name == name:
+        link_id = o.link
         if link_id:
           link = self.global_state.id2link[link_id]
           return self.global_state.id2node[link.origin_node_id]
@@ -78,8 +98,8 @@ class Node:
 
   def getInputLinkByName(self, name):
     for o in self.inputs:
-      if o["name"] == name:
-        link_id = o["link"]
+      if o.name == name:
+        link_id = o.link
         if link_id:
           return self.global_state.id2link[link_id]
     return None
@@ -126,7 +146,7 @@ class BackBufferNode(Node):
     gl.glDepthFunc(gl.GL_LEQUAL)
     gl.glBlendFunc(gl.GL_ONE, gl.GL_ONE)
 
-    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, null)
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, None)
     gl.glDrawBuffers([gl.GL_BACK])
     gl.glViewport(0, 0, global_state.width, global_state.height)
     gl.glClearColor(0, 0, 1, 1)
@@ -186,47 +206,158 @@ class FeedbackNode(Node):
     self.is_recursive = True
 
 
+class PassNodeGL:
+  def __init__(self):
+    self.fb = None
+    self.rts = []
+    self.depth = None
+    self.draw_buffers = []
+
+
 class PassNode(Node):
   def __init__(self, global_state, json_node):
     super().__init__(global_state, json_node)
+    self.gl = PassNodeGL()
     assert("viewport" in self.properties and (
         "rts" in self.properties or "depth" in self.properties))
 
   def gl_render(self):
-    self.bind(gl)
+    self.bind()
     for i, input in enumerate(self.inputs):
       if input["type"] == "drawcall_t":
-        node = get_input_node_by_slot(i)
+        node = self.get_input_node_by_slot(i)
         node.gl_draw()
 
+  def gl_init(self):
+    self.gl = PassNodeGL()
+    self.gl.fb = gl.glGenFramebuffers(1);
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.gl.fb);
+    for i in range(0, len(self.properties.rts)):
+      tex = self.gen_texture(i)
+      self.gl.rts.append(tex)
+      self.gl.draw_buffers.append(gl.GL_COLOR_ATTACHMENT0 + i)
+      gl.glFramebufferTexture2D(
+          gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0 + i, gl.GL_TEXTURE_2D, tex, 0)
+
+    if self.properties.depth != None:
+      tex = self.gen_texture(999)
+      self.gl.depth = tex
+      gl.glFramebufferTexture2D(
+          gl.GL_FRAMEBUFFER, gl.GL_DEPTH_ATTACHMENT, gl.GL_TEXTURE_2D, tex, 0)
+
+  def clone_texture(self, id):
+    targetTextureWidth = self.properties.viewport.width
+    targetTextureHeight = self.properties.viewport.height
+    targetTexture = self.gen_texture(gl, id)
+    srcTexture = self.get_texture(id)
+    fb = gl.glGenFramebuffers(1)
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, fb)
+    gl.glFramebufferTexture2D(
+        gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, targetTexture, 0)
+
+    gl.glDisable(gl.GL_CULL_FACE)
+    gl.glDisable(gl.GL_DEPTH_TEST)
+    gl.glDisable(gl.GL_DEPTH_TEST)
+    gl.glDisable(gl.GL_SCISSOR_TEST)
+    gl.glDepthFunc(gl.GL_LEQUAL)
+    gl.glBlendFunc(gl.GL_ONE, gl.GL_ONE)
+
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, fb)
+    gl.glDrawBuffers([gl.GL_COLOR_ATTACHMENT0])
+    gl.glViewport(0, 0, targetTextureWidth, targetTextureHeight)
+    gl.glClearColor(0, 0, 1, 1)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+    global_state.render_texture(srcTexture)
+
+    gl.glDeleteFramebuffers(1, fb)
+    return targetTexture
+
+  def gen_texture(self, id):
+    if id >= len(self.properties.rts):
+      rt = self.properties.depth
+      tex = gl.glGenTextures(1)
+      gl.glBindTexture(gl.GL_TEXTURE_2D, tex)
+      format = None
+      if rt.format == "D16":
+        format = gl.GL_DEPTH_COMPONENT16
+      elif rt.format == "D32":
+        format = gl.GL_DEPTH_COMPONENT32F
+      else:
+        raise "unknown format"
+
+      gl.glTexStorage2D(gl.GL_TEXTURE_2D, 1, format,
+        self.properties.viewport.width, self.properties.viewport.height)
+      return tex
+    else:
+      rt = self.properties.rts[id]
+      tex = gl.glGenTextures(1)
+      gl.glBindTexture(gl.GL_TEXTURE_2D, tex)
+      format = None
+      if rt.format == "RGBA8":
+        format = gl.GL_RGBA8
+      elif rt.format == "RGBA32F":
+        format = gl.GL_RGBA32F
+      else:
+        raise "unknown format"
+      gl.glTexStorage2D(gl.GL_TEXTURE_2D, 1, format,
+        self.properties.viewport.width, self.properties.viewport.height)
+      return tex
+
+  def get_texture(self, id):
+    if id >= len(self.properties.rts):
+      return self.gl.depth
+    return self.gl.rts[id]
+
+  def bind(self):
+    gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.gl.fb)
+    gl.glDrawBuffers(self.gl.draw_buffers)
+    gl.glViewport(0, 0, self.properties.viewport.width,
+                  self.properties.viewport.height)
+    gl.glClearColor(0, 0, 0, 1)
+    gl.glClearDepth(1.0)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+  def gl_release(self):
+    if self.gl.fb != None:
+      gl.glDeleteFramebuffers(1, self.gl.fb)
+
+    for i in range(0, len(self.gl.rts)):
+      tex=self.gl.rts[i]
+      gl.glDeleteTextures(1, tex)
+
+    if self.gl.depth != None:
+      gl.glDeleteTextures(1, self.gl.depth)
+
+    self.gl=PassNodeGL()
 
 class GlobalState:
   def __init__(self):
-    self.nodes = []
-    self.id2node = {}
-    self.links = []
-    self.id2link = {}
-    self.frame_count = 0
+    self.nodes=[]
+    self.id2node={}
+    self.links=[]
+    self.id2link={}
+    self.frame_count=0
     pass
 
   def toposort(self):
     """
     Returns the list of nodes in inverse topological order
     """
-    sorted = []
-    unsorted = []
-    unsorted_set = set()
+    sorted=[]
+    unsorted=[]
+    unsorted_set=set()
     for node in self.nodes:
       if node.is_recursive:
         continue
       unsorted_set.add(node.id)
       unsorted.append(node)
     while len(unsorted_set) != 0:
-      node = unsorted.pop(0)
-      is_ready = True
+      node=unsorted.pop(0)
+      is_ready=True
       for id in node.get_input_ids():
         if id in unsorted_set:
-          is_ready = False
+          is_ready=False
           break
       if is_ready:
         sorted.append(node)
@@ -239,32 +370,32 @@ class GlobalState:
     return sorted
 
   def load_json(self, filename):
-    self.json = json.load(open(filename))
+    self.json=AD(json.load(open(filename)))
     self.release()
-    self.nodes = []
-    self.id2node = {}
-    self.links = []
-    self.id2link = {}
+    self.nodes=[]
+    self.id2node={}
+    self.links=[]
+    self.id2link={}
     assert("nodes" in self.json)
     for json_node in self.json["nodes"]:
       if json_node["type"] == "gfx/PassNode":
-        self.nodes.append(PassNode(self, json_node))
+        self.nodes.append(PassNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/BackBufferNode":
-        self.nodes.append(BackBufferNode(self, json_node))
+        self.nodes.append(BackBufferNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/DrawCallNode":
-        self.nodes.append(DrawCallNode(self, json_node))
+        self.nodes.append(DrawCallNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/PipelineNode":
-        self.nodes.append(PipelineNode(self, json_node))
+        self.nodes.append(PipelineNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/VertexBufferNode":
-        self.nodes.append(VertexBufferNode(self, json_node))
+        self.nodes.append(VertexBufferNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/ModelNode":
-        self.nodes.append(ModelNode(self, json_node))
+        self.nodes.append(ModelNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/TextureBufferNode":
-        self.nodes.append(TextureBufferNode(self, json_node))
+        self.nodes.append(TextureBufferNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/FeedbackNode":
-        self.nodes.append(FeedbackNode(self, json_node))
+        self.nodes.append(FeedbackNode(self, AD(json_node)))
       elif json_node["type"] == "gfx/FrameCountNode":
-        self.nodes.append(FrameCountNode(self, json_node))
+        self.nodes.append(FrameCountNode(self, AD(json_node)))
       else:
         assert(False and "unknown node type")
 
@@ -272,7 +403,7 @@ class GlobalState:
       self.links.append(Link(self, json_link))
 
   def render_triangle(self):
-    vsSource = """#version 300 es
+    vsSource="""#version 300 es
 
       layout (location=0) in vec4 position;
       layout (location=1) in vec3 color;
@@ -285,7 +416,7 @@ class GlobalState:
           gl_Position = position;
       }
       """
-    fsSource = """#version 300 es
+    fsSource="""#version 300 es
       precision highp float;
       in vec3 vColor;
       out vec4 fragColor;
@@ -295,36 +426,36 @@ class GlobalState:
       }
       """
 
-    VERTEX_SHADER = shaders.compileShader(vsSource, gl.GL_VERTEX_SHADER)
+    VERTEX_SHADER=shaders.compileShader(vsSource, gl.GL_VERTEX_SHADER)
 
-    FRAGMENT_SHADER = shaders.compileShader(fsSource, gl.GL_FRAGMENT_SHADER)
+    FRAGMENT_SHADER=shaders.compileShader(fsSource, gl.GL_FRAGMENT_SHADER)
 
-    program = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+    program=shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
 
     gl.glUseProgram(program)
 
-    triangleArray = gl.glGenVertexArrays(1)
+    triangleArray=gl.glGenVertexArrays(1)
     gl.glBindVertexArray(triangleArray)
 
-    positions = np.float32([
+    positions=np.float32([
         -0.5, -0.5, 0.0,
         0.5, -0.5, 0.0,
         0.0, 0.5, 0.0
     ])
 
-    positionBuffer = gl.glGenBuffers(1)
+    positionBuffer=gl.glGenBuffers(1)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, positionBuffer)
     gl.glBufferData(gl.GL_ARRAY_BUFFER, positions, gl.GL_STATIC_DRAW)
     gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, False, 0, None)
     gl.glEnableVertexAttribArray(0)
 
-    colors = np.float32([
+    colors=np.float32([
         1.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
         1.0, 0.0, 1.0
     ])
 
-    colorBuffer = gl.glGenBuffers(1)
+    colorBuffer=gl.glGenBuffers(1)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, colorBuffer)
     gl.glBufferData(gl.GL_ARRAY_BUFFER, colors, gl.GL_STATIC_DRAW)
     gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, False, 0, None)
@@ -348,7 +479,7 @@ class GlobalState:
     """
     Renders a fullscreen quad with the given texture
     """
-    vsSource = """#version 300 es
+    vsSource="""#version 300 es
       precision highp float;
         in vec2 position;
         out vec2 uv;
@@ -358,7 +489,7 @@ class GlobalState:
             gl_Position = vec4(position, 0, 1);
         }
       """
-    fsSource = """#version 300 es
+    fsSource="""#version 300 es
       precision highp float;
       precision highp int;
       precision highp usampler2D;
@@ -371,10 +502,10 @@ class GlobalState:
       }
       """
     if flipy:
-      fsSource = fsSource.replace("uv.y = 1.0 - uv.y;", "uv.y = uv.y;")
+      fsSource=fsSource.replace("uv.y = 1.0 - uv.y;", "uv.y = uv.y;")
     if format:
       if format == "RGBA32UI":
-        fsSource = fsSource.replace(
+        fsSource=fsSource.replace(
             "uniform sampler2D", "uniform usampler2D")
       elif format == "RGBA32F":
         pass
@@ -383,20 +514,20 @@ class GlobalState:
       else:
         raise "unknown format"
 
-    VERTEX_SHADER = shaders.compileShader(vsSource, gl.GL_VERTEX_SHADER)
+    VERTEX_SHADER=shaders.compileShader(vsSource, gl.GL_VERTEX_SHADER)
 
-    FRAGMENT_SHADER = shaders.compileShader(fsSource, gl.GL_FRAGMENT_SHADER)
+    FRAGMENT_SHADER=shaders.compileShader(fsSource, gl.GL_FRAGMENT_SHADER)
 
-    program = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+    program=shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
 
     gl.glUseProgram(program)
 
-    triangleArray = gl.glGenVertexArrays(1)
+    triangleArray=gl.glGenVertexArrays(1)
     gl.glBindVertexArray(triangleArray)
 
-    data = np.float32([-4, -4, 4, -4, 0, 4])
+    data=np.float32([-4, -4, 4, -4, 0, 4])
 
-    positionBuffer = gl.glGenBuffers(1)
+    positionBuffer=gl.glGenBuffers(1)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, positionBuffer)
     gl.glBufferData(gl.GL_ARRAY_BUFFER, data, gl.GL_STATIC_DRAW)
     gl.glVertexAttribPointer(0, 2, gl.GL_FLOAT, False, 0, None)
@@ -428,21 +559,21 @@ class GlobalState:
     gl.glDeleteProgram(program)
 
   def create_texture(self, data, width, height, format="RGBA8UN"):
-    texture = gl.glGenTextures(1)
+    texture=gl.glGenTextures(1)
     gl.glBindTexture(gl.GL_TEXTURE_2D, texture)
     assert(format == "RGBA8UN")
-    level = 0
-    internalFormat = gl.GL_RGBA
-    border = 0
-    format = gl.GL_RGBA
-    type = gl.GL_UNSIGNED_BYTE
+    level=0
+    internalFormat=gl.GL_RGBA
+    border=0
+    format=gl.GL_RGBA
+    type=gl.GL_UNSIGNED_BYTE
     gl.glTexImage2D(gl.GL_TEXTURE_2D, level, internalFormat,
                     width, height, border,
                     format, type, data)
     return texture
 
   def get_texture_data(self, tex, format, width, height):
-    targetTexture = gl.glGenTextures(1)
+    targetTexture=gl.glGenTextures(1)
     gl.glBindTexture(gl.GL_TEXTURE_2D, targetTexture)
 
     gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA,
@@ -458,7 +589,7 @@ class GlobalState:
         gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
 
     # Create and bind the framebuffer
-    fb = gl.glGenFramebuffers(1)
+    fb=gl.glGenFramebuffers(1)
     gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, fb)
     gl.glFramebufferTexture2D(
         gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, targetTexture, 0)
@@ -478,9 +609,9 @@ class GlobalState:
 
     self.render_texture(tex, format)
 
-    img_buf = gl.glReadPixelsub(
+    img_buf=gl.glReadPixelsub(
         0, 0, width, height, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE)
-    img = np.frombuffer(img_buf, np.uint8).reshape(height, width, 4)[::-1]
+    img=np.frombuffer(img_buf, np.uint8).reshape(height, width, 4)[::-1]
 
     gl.glDeleteFramebuffers(1, fb)
     gl.glDeleteTextures(1, targetTexture)
@@ -490,7 +621,7 @@ class GlobalState:
     """
     Evaluates the frame graph
     """
-    sorted = self.toposort()
+    sorted=self.toposort()
     for node in sorted:
       if hasattr(node, 'gl_init'):
         node.gl_init()
@@ -507,7 +638,7 @@ class GlobalState:
     pass
 
 
-global_state = GlobalState()
+global_state=GlobalState()
 global_state.load_json("public/default_graph.json")
 
 
@@ -524,11 +655,11 @@ def showScreen():
     # ]), 2, 2)
     # global_state.render_triangle()
     global_state.render()
-    img_buf = gl.glReadPixelsub(
+    img_buf=gl.glReadPixelsub(
         0, 0, WIDTH, HEIGHT, gl.GL_RGB, gl.GL_UNSIGNED_BYTE)
-    img = np.frombuffer(img_buf, np.uint8).reshape(HEIGHT, WIDTH, 3)[::-1]
-    fig, ax = plt.subplots()
-    im = ax.imshow(img/255, interpolation='bilinear',
+    img=np.frombuffer(img_buf, np.uint8).reshape(HEIGHT, WIDTH, 3)[::-1]
+    fig, ax=plt.subplots()
+    im=ax.imshow(img/255, interpolation='bilinear',
                    origin='upper')
     # im = ax.imshow(global_state.get_texture_data(tex, "RGBA8UN", 2, 2)/255, interpolation='bilinear',
     #                origin='upper')
@@ -547,7 +678,7 @@ glutInit()
 glutInitDisplayMode(GLUT_RGBA)
 glutInitWindowSize(WIDTH, HEIGHT)
 glutInitWindowPosition(0, 0)
-wind = glutCreateWindow("OpenGL Coding Practice")
+wind=glutCreateWindow("OpenGL Coding Practice")
 glutDisplayFunc(showScreen)
 glutIdleFunc(showScreen)
 glutMainLoop()
