8ee22ad9b4fee041c004981f2fc2ff9969558054
Anton Schreiner <antonschrein@gmail.com>
2020-02-19 15:49:27 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(
     name='frame_graph',
-    py_modules=['renderpy.py'],
+    packages=['renderpy'],
+    version="0.0.0",
+    author="aschrein",
     url='https://github.com/aschrein/WebThingy',
     license='LICENSE',
 )
